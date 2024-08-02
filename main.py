0048f39458db4360d3f843be3f278b16766c9b21
HillaShx <hilla.sh@gmail.com>
2024-08-02 16:18:13 +0300

    senior passengers

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from datetime import datetime
-from find_winning_player import Solution, input_data
+from count_senior_passengers import Solution, input_data
 # from max_score_for_removing_subs_old import Solution, input_data
 
 
 def main():
-    solution = Solution()
     for i in input_data.keys():
         start_time = datetime.now()
-        result = solution.losingPlayer(**input_data[i])
+        result = Solution().countSeniors(**input_data[i])
         end_time = datetime.now()
         print(f"input {i} | result: {result} | runtime: {end_time-start_time}")
 
